155b48ef7f46a5fcd28d61e9b6d45ae990495b43
Adam <bielaa3@gmail.com>
2025-12-03 15:31:15 +0100

    Deployed 91eca6d with MkDocs version: 1.6.1

--- js/mermaid-init.js
+++ js/mermaid-init.js
@@ -1,12 +1,26 @@
+document.addEventListener("DOMContentLoaded", function() {
+    if (typeof mermaid !== "undefined") {
+        mermaid.initialize({
+            startOnLoad: true,
+            theme: "default"     // możesz zmienić na "forest", "dark", etc.
+        });
+    }
+});
+
+
+
+
 // mermaid.initialize({ startOnLoad: true });
+
+
 
 // window.addEventListener("DOMContentLoaded", () => {
 // mermaid.initialize({ startOnLoad: true });
 // });
 
-document.addEventListener("DOMContentLoaded", function () {
-  if (window.mermaid) {
-    mermaid.initialize({ startOnLoad: true });
-    mermaid.run();
-  }
-});
+// document.addEventListener("DOMContentLoaded", function () {
+//   if (window.mermaid) {
+//     mermaid.initialize({ startOnLoad: true });
+//     mermaid.run();
+//   }
+// });

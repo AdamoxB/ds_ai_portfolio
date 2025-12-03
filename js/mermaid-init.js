020d516ec53b40dcbd14ac10a000034272dbbbbe
Adam <bielaa3@gmail.com>
2025-12-03 14:23:09 +0100

    Deployed 513b962 with MkDocs version: 1.6.1

--- js/mermaid-init.js
+++ js/mermaid-init.js
@@ -1,4 +1,12 @@
 // mermaid.initialize({ startOnLoad: true });
-window.addEventListener("DOMContentLoaded", () => {
-mermaid.initialize({ startOnLoad: true });
+
+// window.addEventListener("DOMContentLoaded", () => {
+// mermaid.initialize({ startOnLoad: true });
+// });
+
+document.addEventListener("DOMContentLoaded", function () {
+  if (window.mermaid) {
+    mermaid.initialize({ startOnLoad: true });
+    mermaid.run();
+  }
 });
